af21c0b9ddd182ad693026c8feaf278e63b2ae02
airsoftdude4lif3 <144823364+airsoftdude4lif3@users.noreply.github.com>
2023-11-15 08:15:58 -0500

    fix color rockets

--- rocket.js
+++ rocket.js
@@ -3,8 +3,8 @@
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/bGz7mv2vD6g
 
-Const COLORS =[ color (0,102,204,),
-               color (255,102,52),
+const COLORS =[ color (0,102,204,)
+               color (255,102,52)
       ];
 
 
