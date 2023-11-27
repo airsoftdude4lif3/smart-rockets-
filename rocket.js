683c84c800a663c2ab14150a75aba86b84a0a037
airsoftdude4lif3 <144823364+airsoftdude4lif3@users.noreply.github.com>
2023-11-27 11:31:42 -0500

    Update rocket color 7

--- rocket.js
+++ rocket.js
@@ -4,8 +4,8 @@
 // Code for: https://youtu.be/bGz7mv2vD6g
 
 const COLORS =[ [0,102,204],
-                [255,102,52]
-                [178,102,255] ];
+                [255,102,52],
+                [178,102,255], ];
 
 
 // Constructor function
